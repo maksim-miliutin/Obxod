ace0014729304e86fe039f20cf7171c6285d8146
Maksim Miliutin <maksim.milutin06@gmail.com>
2026-09-25 11:39:56 +0300

    raise engine from button

--- cmd/obxod-ui/main.go
+++ cmd/obxod-ui/main.go
@@ -5,28 +5,55 @@ package main
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
+
+	"obxod/internal/preset"
+	"obxod/internal/runner"
 )
 
 func main() {
 	window := app.New().NewWindow("Obxod")
 
-	on := false
+	var session *runner.Session
 	button := widget.NewButton("Включить", nil)
 	button.OnTapped = func() {
-		on = !on
-
-		if on {
-			button.SetText("Выключить")
+		if session != nil {
+			session.Stop()
+			session = nil
+			button.SetText("Включить")
 
 			return
 		}
 
-		button.SetText("Включить")
+		started, err := start()
+		if err != nil {
+			dialog.ShowError(err, window)
+
+			return
+		}
+
+		session = started
+		go session.Run()
+		button.SetText("Выключить")
 	}
 
 	window.SetContent(button)
 	window.Resize(fyne.NewSize(360, 420))
 
 	window.ShowAndRun()
+}
+
+func start() (*runner.Session, error) {
+	set, err := preset.All()[0].Rules()
+	if err != nil {
+		return nil, err
+	}
+
+	return runner.Open(runner.Config{
+		Rules:    set,
+		Wet:      true,
+		DropQUIC: true,
+		Report:   func(string) {},
+	})
 }
